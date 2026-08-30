9689c109d06a9d941562f58bba76ddf52c2d9661
DanielSousa07 <josenidaniel88@gmail.com>
2026-08-30 19:02:34 -0300

    Mudando o resend from

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM_EMAIL = process.env.RESEND_FROM || "Ludus <onboarding@lives.systems>";
+const FROM_EMAIL = process.env.RESEND_FROM || "Ludus <onboarding@cocaistech.com>";
 
 
 function getHtmlTemplate(title: string, content: string) {
